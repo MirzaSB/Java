e382777dd49b43c5f605bbd4d2b10dd79831333c
mirzasb <mirzasbaig@nyu.edu>
2016-03-23 20:07:52 -0400

    Completed the mongoDB chapter

--- udemy/h2demo/src/main/java/com/therealdanvega/repository/PostRepository.java
+++ udemy/h2demo/src/main/java/com/therealdanvega/repository/PostRepository.java
@@ -5,9 +5,15 @@ import org.springframework.stereotype.Repository;
 
 import com.therealdanvega.domain.Post;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
 
     // Find the latest blog post to display on the home page.
     Post findFirstByOrderByPostedOnDesc();
+
+    List<Post> findAllByOrderByPostedOnDesc();
+
+    Post findBySlug(String slug);
 }
--- udemy/h2demo/src/main/java/com/therealdanvega/service/PostService.java
+++ udemy/h2demo/src/main/java/com/therealdanvega/service/PostService.java
@@ -5,17 +5,27 @@ import com.therealdanvega.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostService {
 
     private PostRepository postRepository;
 
     @Autowired
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository){
         this.postRepository = postRepository;
     }
 
-    public Post getLatestPost() {
+    public Post getLatestPost(){
         return postRepository.findFirstByOrderByPostedOnDesc();
+    }
+
+    public List<Post> list() {
+        return postRepository.findAllByOrderByPostedOnDesc();
+    }
+
+    public Post getBySlug(String slug) {
+        return postRepository.findBySlug(slug);
     }
 }
